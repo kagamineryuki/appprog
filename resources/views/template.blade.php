bb736992e111f75b53e0dd206f3b39d7f3ee949a
Stanislaus Christian Harjanto <16k10002@student.unika.ac.id>
2018-09-29 19:36:35 +0700

    nambah login screen dan images buat bg

---
+++ resources/views/template.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{config('app.name')}}</title>
+    @yield('header')
+</head>
+
+<body>
+    @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teacher/dashboard', function () {
+   return view('teacher.dashboard');
+});
